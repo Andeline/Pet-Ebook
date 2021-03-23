f2f16408c3cb93e004ce95265251faca5297a0a0
Andeline <andelineya@gmail.com>
2021-03-23 18:16:49 +0800

    毛的世界電子書 My first Ebook

--- Pet Ebook/ViewController.swift
+++ Pet Ebook/ViewController.swift
@@ -11,6 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let barAppearance =  UINavigationBarAppearance()
+        barAppearance.configureWithTransparentBackground()
+        navigationController?.navigationBar.standardAppearance = barAppearance
+     
         // Do any additional setup after loading the view.
     }
 
